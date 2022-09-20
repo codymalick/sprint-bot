c95c10b4cd6847df3b5130f1e3fea967ce3029f6
codymalick <cody.malick@gmail.com>
2022-09-19 22:10:11 -0700

    Add delay, notify args
    /sprint can now notify (@here) the entire channel. It can also be
    started with a delay in minutes.

--- commands/sprint.js
+++ commands/sprint.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
 
 
 module.exports = {
@@ -7,17 +8,47 @@ module.exports = {
 		.setDescription('Set a timer for a writing sprint!')
         .addIntegerOption(option => 
             option.setName('time')
-                .setDescription('Length of sprint in minutes')
-                .setRequired(true)),
+                .setDescription('Length of sprint (minutes)')
+                .setRequired(true))
+        .addIntegerOption(option =>
+            option.setName('delay')
+                .setDescription('Delay before the start of the sprint (minutes)')
+                .setRequired(false)) 
+        .addBooleanOption(option => 
+            option.setName('notify')
+                .setDescription('Notify the channel using @here that the sprint is starting')
+                .setRequired(false)),
 
 	async execute(interaction) {
         const minutes = interaction.options.getInteger('time')
-        const seconds = minutes * 60 // minutes * 60
+        const toMs = (minutes) => minutes * 60 * 1000
+
+        const delay = interaction.options.getInteger('delay') ?? 0
+        const notify = interaction.options.getBoolean('notify') ?? false
 
         console.log(`Starting a ${minutes} minute sprint`)
-        await interaction.reply(`Starting ${minutes} minute(s) sprint!`)
+        if(notify)
+        {
+            if(delay !== 0)
+            {
+                await interaction.reply(`@here a ${minutes} minute(s) sprint is starting in ${minutes} minutes(s)`)
+                await wait(toMs(minutes))
+                await interaction.followUp(`Sprint starting!!`)
+            } else {
+                await interaction.reply(`@here starting ${minutes} minute(s) sprint!`)
+            }
 
-        await new Promise(resolve => setTimeout(resolve, seconds * 1000))
+        } else {
+            if(delay !== 0)
+            {
+                await interaction.reply(`Starting a ${minutes} minute(s) sprint is starting in `)
+                await wait(toMs(minutes))
+                await interaction.followUp(`Sprint starting!!`)
+            } else {
+                await interaction.reply(`Starting ${minutes} minute(s) sprint!`)
+            }
+        }
+        await wait(toMs(minutes))
 		await interaction.followUp('The sprint is complete!')
 	},
 };
